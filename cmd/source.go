e88f714d694788bfdd001794ee34b1fd1cc50b24
Michael Lavers <kolanos@gmail.com>
2018-09-22 12:21:41 -0700

    Adding more source commands

--- cmd/source.go
+++ cmd/source.go
@@ -7,6 +7,10 @@ import (
 	"github.com/spf13/cobra"
 )
 
+var sourceDepositVerifyAmount1 string
+var sourceDepositVerifyAmount2 string
+var sourceDepositVerifyCurrency string
+
 var sourceUpdateAccountNumber string
 var sourceUpdateBankAccountType string
 var sourceUpdateName string
@@ -15,6 +19,123 @@ var sourceUpdateRoutingNumber string
 var sourceCmd = &cobra.Command{
 	Use:   "source",
 	Short: "Funding source management",
+}
+
+var sourceBalanceCmd = &cobra.Command{
+	Use:   "balance",
+	Short: "Balance management",
+}
+
+var sourceBalanceRetrieveCmd = &cobra.Command{
+	Use:   "retrieve",
+	Short: "Retrieve balance",
+	Args:  cobra.ExactArgs(1),
+	Run: func(cmd *cobra.Command, args []string) {
+		initClient()
+
+		src, err := client.FundingSource.Retrieve(args[0])
+
+		if err != nil {
+			renderError(err)
+		}
+
+		res, err := src.RetrieveBalance()
+
+		if err != nil {
+			renderError(err)
+		}
+
+		renderResource(res)
+	},
+}
+
+var sourceDepositCmd = &cobra.Command{
+	Use:   "deposit",
+	Short: "Micro deposit management",
+}
+
+var sourceDepositInitiateCmd = &cobra.Command{
+	Use:   "initiate",
+	Short: "Initiate micro deposits",
+	Args:  cobra.ExactArgs(1),
+	Run: func(cmd *cobra.Command, args []string) {
+		initClient()
+
+		src, err := client.FundingSource.Retrieve(args[0])
+
+		if err != nil {
+			renderError(err)
+		}
+
+		res, err := src.InitiateMicroDeposits()
+
+		if err != nil {
+			renderError(err)
+		}
+
+		renderResource(res)
+	},
+}
+
+var sourceDepositRetrieveCmd = &cobra.Command{
+	Use:   "retrieve",
+	Short: "Retrieve micro deposits",
+	Args:  cobra.ExactArgs(1),
+	Run: func(cmd *cobra.Command, args []string) {
+		initClient()
+
+		src, err := client.FundingSource.Retrieve(args[0])
+
+		if err != nil {
+			renderError(err)
+		}
+
+		res, err := src.RetrieveMicroDeposits()
+
+		if err != nil {
+			renderError(err)
+		}
+
+		renderResource(res)
+	},
+}
+
+var sourceDepositVerifyCmd = &cobra.Command{
+	Use:   "verify",
+	Short: "Verify micro deposits",
+	Args:  cobra.ExactArgs(1),
+	Run: func(cmd *cobra.Command, args []string) {
+		initClient()
+
+		src, err := client.FundingSource.Retrieve(args[0])
+
+		if err != nil {
+			renderError(err)
+		}
+
+		var currency dwolla.Currency
+		switch sourceDepositVerifyCurrency {
+		case "USD":
+			currency = dwolla.USD
+		default:
+			renderError(fmt.Errorf("Invalid currency: %s", sourceDepositVerifyCurrency))
+		}
+
+		err = src.VerifyMicroDeposits(&dwolla.MicroDepositRequest{
+			Amount1: dwolla.Amount{
+				Value:    sourceDepositVerifyAmount1,
+				Currency: currency,
+			},
+			Amount2: dwolla.Amount{
+				Value:    sourceDepositVerifyAmount2,
+				Currency: currency,
+			},
+		})
+
+		if err != nil {
+			renderError(err)
+		}
+	},
 }
 
 var sourceRemoveCmd = &cobra.Command{
@@ -67,7 +188,7 @@ var sourceUpdateCmd = &cobra.Command{
 			case "savings":
 				bankAccountType = dwolla.FundingSourceBankAccountTypeSavings
 			default:
-				renderError(fmt.Errorf("Invalid bank account type: %s", accountSourceCreateBankAccountType))
+				renderError(fmt.Errorf("Invalid bank account type: %s", sourceUpdateBankAccountType))
 			}
 		}
 
@@ -89,12 +210,26 @@ var sourceUpdateCmd = &cobra.Command{
 func init() {
 	rootCmd.AddCommand(sourceCmd)
 
+	sourceCmd.AddCommand(sourceBalanceCmd)
+	sourceCmd.AddCommand(sourceDepositCmd)
 	sourceCmd.AddCommand(sourceRemoveCmd)
 	sourceCmd.AddCommand(sourceRetrieveCmd)
 	sourceCmd.AddCommand(sourceUpdateCmd)
 
-	sourceUpdateCmd.Flags().StringVar(&accountSourceCreateAccountNumber, "account-number", "", "bank account number")
-	sourceUpdateCmd.Flags().StringVar(&accountSourceCreateBankAccountType, "account-type", "", "bank account type")
-	sourceUpdateCmd.Flags().StringVar(&accountSourceCreateName, "name", "", "bank account nickname")
-	sourceUpdateCmd.Flags().StringVar(&accountSourceCreateRoutingNumber, "routing-number", "", "bank routing number")
+	sourceBalanceCmd.AddCommand(sourceBalanceRetrieveCmd)
+
+	sourceDepositCmd.AddCommand(sourceDepositInitiateCmd)
+	sourceDepositCmd.AddCommand(sourceDepositRetrieveCmd)
+	sourceDepositCmd.AddCommand(sourceDepositVerifyCmd)
+
+	sourceDepositVerifyCmd.Flags().StringVar(&sourceDepositVerifyAmount1, "amount1", "", "first micro deposit amount (required)")
+	sourceDepositVerifyCmd.MarkFlagRequired("amount1")
+	sourceDepositVerifyCmd.Flags().StringVar(&sourceDepositVerifyAmount2, "amount2", "", "second micro deposit amount (required)")
+	sourceDepositVerifyCmd.MarkFlagRequired("amount2")
+	sourceDepositVerifyCmd.Flags().StringVar(&sourceDepositVerifyCurrency, "currency", "USD", "micro deposit currency")
+
+	sourceUpdateCmd.Flags().StringVar(&sourceUpdateAccountNumber, "account-number", "", "bank account number")
+	sourceUpdateCmd.Flags().StringVar(&sourceUpdateBankAccountType, "account-type", "", "bank account type")
+	sourceUpdateCmd.Flags().StringVar(&sourceUpdateName, "name", "", "bank account nickname")
+	sourceUpdateCmd.Flags().StringVar(&sourceUpdateRoutingNumber, "routing-number", "", "bank routing number")
 }
